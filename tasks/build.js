019d571babfe25da39663120d989967c959fb4a4
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-08-07 18:31:30 +0200

    fixed build

--- tasks/build.js
+++ tasks/build.js
@@ -2,5 +2,8 @@
 
 var Task = require('wheelie/lib/models/task');
 
+var dependencies = [
+  'sass', 'assets', 'templates', 'handlebars'
+];
 
-module.exports = new Task('build', ['sass', 'assets', 'templates', 'handlebars']);
+module.exports = new Task('build', dependencies);
