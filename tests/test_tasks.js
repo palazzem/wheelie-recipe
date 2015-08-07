4b3b6e5b51739ed613b3304d0c2578ad802d55ba
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-08-07 11:47:03 +0200

    testing tasks loading

---
+++ tests/test_tasks.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var wheelie = require('wheelie');
+var recipe = require('../index');
+
+describe('The recipe', function() {
+  it('should provide a set of tasks', function() {
+    expect(recipe.length).to.be.equal(7);
+  });
+});
