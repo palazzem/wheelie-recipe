6febdddf3b2998475f335c3906b5b55bf868cb2b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-06 20:00:58 +0200

    fixed #15: using Jest test framework

--- __tests__/test_tasks.js
+++ __tests__/test_tasks.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var wheelie = require('wheelie');
 var recipe = require('../index');
 
+
 describe('The recipe', function() {
   it('should provide a set of tasks', function() {
-    expect(recipe.length).to.be.equal(9);
+    expect(recipe.length).toBe(9);
   });
 });
--- tests/setup.js
+++
@@ -1,6 +0,0 @@
-// add here all common configurations
-
-// loading test framework assert library
-var chai = require('chai');
-
-global.expect = chai.expect;
