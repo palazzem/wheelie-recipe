c84751747985ec332c350d2d04785b71418e11b0
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-07 11:43:24 +0200

    fixed #14: add deprecation warning about using wheelie-recipe instead of wheelie-recipes package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var logger = require('wheelie/lib/logging/logger');
 var assets = require('./tasks/assets');
 var browserSync = require('./tasks/browser_sync');
 var build = require('./tasks/build');
@@ -7,6 +8,10 @@ var jshint = require('./tasks/jshint');
 var sass = require('./tasks/sass');
 var uglify =require('./tasks/uglify');
 var templates = require('./tasks/templates');
+
+
+// deprecation warning
+logger.warning('[DEPRECATION] the recipe `wheelie-recipe` is deprecated; please migrate to `wheelie-recipes`');
 
 module.exports = [
   build,
